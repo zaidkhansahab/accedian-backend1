562ae8438ae22e61c910c42112af4094de6407a5
zaidkhansahab <kzaid6008@gmail.com>
2024-07-15 02:10:07 +0530

    commit

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
@@ -21,6 +21,17 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
